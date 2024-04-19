b8aa7f716575e8ca51c3915ef4dc872cb3125360
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-19 13:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const form = document.getElementById("form").addEventListener("submit" , (event) =>{
+	 
+	event.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = parseInt(document.getElementById("age").value);
+
+	if(!name || !age){
+		alert("please fill all the required fields")
+		return
+	}
+
+	const agepromise = new Promise((resolve , reject) =>{
+		setTimeout(() => {
+			if(age > 18){
+				resolve()
+			}
+			else{
+				reject()
+			}
+		},4000)
+	});
+	agepromise.then(()=>{
+		alert(`Welcome, ${name}. You can vote.`)
+	}).catch((error) => {
+		alert(`Oh sorry ${name}. You aren't old enough.`)
+  })
+  });
+	
